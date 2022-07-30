3d595336ceeefa078a26bb4a9923cdf6d93db060
adam <adam@mrrookemaths.com>
2022-07-30 09:16:45 +0100

    try inlining styles

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,8 @@ const config = {
 	// for more information about preprocessors
 	preprocess: preprocess(),
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		inlineStyleThreshold: 1024
 	}
 };
 

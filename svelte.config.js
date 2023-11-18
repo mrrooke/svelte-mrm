dd1d446f879a10165e6e37ccf4a6880f82982aaa
adam <adam@mrrookemaths.com>
2023-11-18 07:39:41 +0000

    Revert vite 5.0

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import { preprocessMeltUI } from '@melt-ui/pp';
 import sequence from 'svelte-sequential-preprocessor';
-import adapter from '@sveltejs/adapter-vercel';
+// import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config}*/
 const config = {
@@ -8,6 +9,7 @@ const config = {
 	// for more information about preprocessors
 	preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
 	kit: {
+		// adapter: adapter()
 		adapter: adapter({
 			runtime: 'nodejs18.x'
 		})

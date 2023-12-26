a5e31d4160d2512c3dd6edba0e38800769323de6
adam <adam@mrrookemaths.com>
2023-12-26 10:18:44 +0000

    migration to SvelteKit 2

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,7 @@ import adapter from '@sveltejs/adapter-vercel';
 
 // import adapter from '@sveltejs/adapter-auto';
 
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config}*/
 const config = {
